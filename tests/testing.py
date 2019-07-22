eb5c0de9b5b803bf68a7b72a08ca35f1197d4a52
Coumes Quentin <qcoumes@etud.u-pem.fr>
2019-07-22 21:56:36 +0200

    Update testing.py

--- tests/testing.py
+++ tests/testing.py
@@ -5,10 +5,10 @@ from django.test.runner import DiscoverRunner
 class DatabaselessTestRunner(DiscoverRunner):
     """A test suite runner that does not set up and tear down a database."""
 
-    def setup_databases(self):
+    def setup_databases(self, *args **kwargs):
         """Overrides DjangoTestSuiteRunner"""
         pass
 
-    def teardown_databases(self, *args):
+    def teardown_databases(self, *args **kwargs):
         """Overrides DjangoTestSuiteRunner"""
         pass
