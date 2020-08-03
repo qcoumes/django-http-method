bd349122b0408d273da4f8d6969196f15cdf2a16
Quentin Coumes <qcoumes@etud.u-pem.fr>
2020-08-03 22:14:06 +0200

    Use pycodestyle instead of pytest pep8 extension

--- django_http_method/templatetags/http_method.py
+++ django_http_method/templatetags/http_method.py
@@ -7,39 +7,39 @@ register = template.Library()
 
 @register.simple_tag
 def http_get():
-    return mark_safe('<input type="hidden" name="_method" value="GET" \>')
+    return mark_safe(r'<input type="hidden" name="_method" value="GET" \>')
 
 
 @register.simple_tag
 def http_post():
-    return mark_safe('<input type="hidden" name="_method" value="POST" \>')
+    return mark_safe(r'<input type="hidden" name="_method" value="POST" \>')
 
 
 @register.simple_tag
 def http_head():
-    return mark_safe('<input type="hidden" name="_method" value="HEAD" \>')
+    return mark_safe(r'<input type="hidden" name="_method" value="HEAD" \>')
 
 
 @register.simple_tag
 def http_put():
-    return mark_safe('<input type="hidden" name="_method" value="PUT" \>')
+    return mark_safe(r'<input type="hidden" name="_method" value="PUT" \>')
 
 
 @register.simple_tag
 def http_delete():
-    return mark_safe('<input type="hidden" name="_method" value="DELETE" \>')
+    return mark_safe(r'<input type="hidden" name="_method" value="DELETE" \>')
 
 
 @register.simple_tag
 def http_patch():
-    return mark_safe('<input type="hidden" name="_method" value="PATCH" \>')
+    return mark_safe(r'<input type="hidden" name="_method" value="PATCH" \>')
 
 
 @register.simple_tag
 def http_options():
-    return mark_safe('<input type="hidden" name="_method" value="OPTIONS" \>')
+    return mark_safe(r'<input type="hidden" name="_method" value="OPTIONS" \>')
 
 
 @register.simple_tag
 def http_trace():
-    return mark_safe('<input type="hidden" name="_method" value="TRACE" \>')
+    return mark_safe(r'<input type="hidden" name="_method" value="TRACE" \>')
